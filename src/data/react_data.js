e0797fa8e2fccf6026a1562e3d87fdf1a3fa919a
heisdinesh <dinesh35382@gmail.com>
2023-06-04 22:41:22 +0530

    react playlist updated

--- src/data/react_data.js
+++ src/data/react_data.js
@@ -1,10 +1,9 @@
 export const react_data =[
-    { id: 1, item: "React Components", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
-    { id: 2, item: "React States", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
-    { id: 3, item: "Routing", isChecked: false, link: "https://youtu.be/nDGA3km5He4" },
-    { id: 4, item: "Virtual DOM", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
-    { id: 5, item: "Fetching Data", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
-    { id: 6, item: "Page Navigation", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
-    { id: 7, item: "React Hooks", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
-    { id: 8, item: "Redux", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
+    { id: 1, item: "React Basics", isChecked: false, link: "https://www.youtube.com/watch?v=Dorf8i6lCuk&ab_channel=Academind" },
+    { id: 2, item: "React Hooks", isChecked: false, link: "https://youtu.be/LlvBzyy-558" },
+    { id: 3, item: "Redux", isChecked: false, link: "https://youtu.be/2kL28Qyw9-0" },
+    { id: 4, item: "Discord Clone", isChecked: false, link: "https://www.youtube.com/live/zc1loX80TX8?feature=share" },
+    { id: 5, item: "React Query", isChecked: false, link: "https://www.youtube.com/playlist?list=PLC3y8-rFHvwjTELCrPrcZlo6blLBUspd2" },
+    { id: 6, item: "E-commerce", isChecked: false, link: "https://youtu.be/AN3t-OmdyKA" },
+   
   ];
